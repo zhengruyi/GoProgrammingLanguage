11f89118d13ef7b0f8f3cb6e813578e587f67e3d
Ruyi ZHENG <standonthpeak@gmail.com>
2021-04-26 20:36:51 +0800

    chapter 8

---
+++ ch8/countdown1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte,1))
+		abort <- struct{}{}
+	}()
+	fmt.Println("commencing countdown")
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown >0; countdown -- {
+		fmt.Println(countdown)
+		select {
+		case <- tick:
+		case <- abort:
+			fmt.Println("Launch aborted")
+			return
+		}
+	}
+	fmt.Println("launch the rocket")
+}
