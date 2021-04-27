b18643a9a800cc715719fec5143fffcd24baf97a
Ruyi ZHENG <standonthpeak@gmail.com>
2021-04-27 20:49:42 +0800

    chapter 8

---
+++ ch8/du.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+)
+var sema = make(chan struct{},20)
+func walkDir(dir string, n * sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subDir := filepath.Join(dir,entry.Name())
+			go walkDir(subDir,n,fileSizes)
+		}else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+func dirents(dir string) [] os.FileInfo {
+	sema <- struct{}{}
+	defer func() {<- sema}()
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"du1: %v\n", err)
+		return nil
+	}
+	return entries
+}
+
+func main() {
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string {"."}
+	}
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root,&n,fileSizes)
+	}
+	go func() {
+		n.Wait()
+		close(fileSizes)
+	}()
+	var nfiles, nbytes int64
+	for size := range fileSizes {
+		nfiles++
+		nbytes += size
+	}
+	printDiskUsage(nfiles, nbytes)
+}
+
+func printDiskUsage(nfiles, nbytes int64)  {
+	fmt.Printf("%d files %.1f GB\n",nfiles, float64(nbytes)/1e9)
+}
