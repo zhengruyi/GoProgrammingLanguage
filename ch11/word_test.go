6881c9c8107091b2641a7d274a6f6105d0789fe5
Ruyi ZHENG <standonthpeak@gmail.com>
2021-05-01 21:43:26 +0800

    chapter 11

--- ch11/word_test.go
+++ ch11/word_test.go
@@ -1,7 +1,9 @@
 package word
 
 import "testing"
-
+/*
+  写几个测试例子，测试文件必须以_test.go，并且所有的测试函数都必须以Test函数开头
+ */
 func TestPalindrome(t *testing.T) {
 	if ! IsPalindrome("detartrated") {
 		t.Error("IsPalindrome(detartrated) = false")
