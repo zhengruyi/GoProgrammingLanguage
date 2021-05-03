b07088e878c1beaf97fa65ee2aecd178b6f5c93f
Ruyi ZHENG <standonthpeak@gmail.com>
2021-05-03 21:35:12 +0800

    chapter 11

--- ch4/treeSort.go
+++ ch4/treeSort.go
@@ -12,7 +12,9 @@ func Sort(values []int)  {
 	}
 	appendValues(values[:0], root)
 }
-
+/*
+中序遍历对搜索二叉树会得到有序序列
+ */
 func appendValues(values []int, t *tree) []int {
 	if t != nil {
 		values = appendValues(values, t.left)
@@ -21,7 +23,10 @@ func appendValues(values []int, t *tree) []int {
 	}
 	return values
 }
+/*
+根据value和当前节点的值，选择将值插在当前节点的左边还是右边
 
+ */
 func add(t * tree, value int) *tree  {
 	if t == nil {
 		t = new (tree)
