785ed397955a670c3af4d4e398d6d13023836377
Ruyi ZHENG <standonthpeak@gmail.com>
2021-05-05 21:37:44 +0800

    pubsub

--- Go语言高级编程/pubsub/pubsub.go
+++ Go语言高级编程/pubsub/pubsub.go
@@ -6,18 +6,25 @@ import (
 	"sync"
 	"time"
 )
-
+/*
+重定义了两种类型，一种可以传递任何类型值的管道和主题函数
+ */
 type(
 	subscriber chan interface{}
 	topicFunc func(v interface{}) bool
 )
+/*
+发布者有四个属性,分别是读写锁，缓存大小和发送的超时时间以及最后的保存全部订阅信息的哈希表
+ */
 type Publisher struct {
 	m sync.RWMutex
 	buffer int
 	timeout time.Duration
 	subscribers map[subscriber]topicFunc
 }
-
+/*
+构造函数，返回一个构造好的Publisher对象
+ */
 func NewPublisher(publishTimeout time.Duration, buffer int) *Publisher {
 	return &Publisher {
 		buffer : buffer,
@@ -28,14 +35,20 @@ func NewPublisher(publishTimeout time.Duration, buffer int) *Publisher {
 func (p *Publisher) SubscribeTopic(topic topicFunc) chan interface{} {
 	return p.SubscribeTopic(nil)
 }
-
+/*
+取消订阅的方式就是加上读写锁，然后将哈希表中的对应条目删去就可以
+最后将对应的管道关闭就行
+ */
 func (p *Publisher) Evict(sub chan interface{})  {
 	p.m.Lock()
 	defer p.m.Unlock()
 	delete(p.subscribers,sub)
 	close(sub)
 }
-
+/*
+广播信息,遍历全部的订阅列表，每次遇到一个管道就启动一个协程
+想这个管道中发送信息
+ */
 func (p *Publisher) Publish(v interface{}) {
 	p.m.RLock()
 	defer p.m.Unlock()
@@ -46,7 +59,9 @@ func (p *Publisher) Publish(v interface{}) {
 	}
 	wg.Wait()
 }
-
+/*
+关闭时遍历所有订阅列表，招聘处其中的管道，然后挨个关闭就行
+ */
 func (p *Publisher) Close()  {
 	p.m.Lock()
 	defer p.m.Unlock()
@@ -59,6 +74,9 @@ func (p *Publisher) Close()  {
 func (p *Publisher) Subscribe() chan interface{} {
 	return p.SubscribeTopic(nil)
 }
+/*
+用select语法来判断是信息发送成功还是超时了
+ */
 func (p *Publisher) sendTopic(sub subscriber, topic topicFunc, v interface{},wg *sync.WaitGroup)  {
 	defer wg.Done()
 	if topic != nil && !topic(v) {
