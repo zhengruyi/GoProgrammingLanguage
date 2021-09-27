3c1ad54450b080a23b9fb4362b72f451f656a2ce
Ruyi ZHENG <standonthpeak@gmail.com>
2021-09-27 21:54:55 +0800

    Q100

---
+++ Leetcode.com/Tree/Q100_Same Tree.go
@@ -0,0 +1,13 @@
+package Tree
+
+
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if (p != nil && q == nil) || (p == nil && q != nil) {
+		return false
+	}
+	if p == nil && q == nil {
+		return true
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
